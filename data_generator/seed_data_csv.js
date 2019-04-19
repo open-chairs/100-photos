50b03f75ec0104b89b69f7be2611b5bcc847ca29
Ray Lin <raylin009@gmail.com>
2019-04-19 10:52:46 -0700

    10m seed script complete

--- data_generator/seed_data_csv.js
+++ data_generator/seed_data_csv.js
@@ -5,31 +5,52 @@ const zeroFill = require('zero-fill');
 
 const writer = csvWriter();
 
+
+
 var randomImage = function () {
     var num = Math.floor(Math.random() * 100) + 1;
     return zeroFill(3, num);
   }
 
-var insertPhotoRow = function (id) {
+writer.pipe(fs.createWriteStream('./sdc_seed_data_1m.csv'))
+
+var insertPhotoRow = function () {
     let imageName = randomImage();
     let url = `./images/00${imageName}.jpg`;
 
     let restaurant_id = Math.floor(Math.random() * 100) + 1;
-    // let restaurant_id = 1;
     let description = faker.lorem.sentence();
     let date = faker.date.past().toString();
     let source = faker.lorem.words();
-    var dataEntry = {id,url,restaurant_id,description,date,source}
+    var dataEntry = {url,restaurant_id,description,date,source}
 
     return dataEntry;
 }
 
-writer.pipe(fs.createWriteStream('sdc_seed_data_1m.csv'))
+const write1mil = ()=>{
+  let i = 10000000;
+  createPhotos();
+  function createPhotos() {
+    let ok = true;
+    do {
+      i--
+      if(i===0) {
+        var data = insertPhotoRow();
 
-for (var i = 1; i <1000000;i++){
-    var data = insertPhotoRow(i);
-    writer.write(data);
+        writer.write(data);
+      } else {
+        var data = insertPhotoRow();
+
+        ok = writer.write(data);
+      }
+    } while (i > 0 && ok);
+    if (i>0) {
+
+      writer.once('drain', createPhotos)
+    }
+  } 
 }
+git
+write1mil()
 
-writer.end()
 
