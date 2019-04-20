ab8149d8db6270e07af5f305b22cd822412492ed
Ray Lin <raylin009@gmail.com>
2019-04-20 16:50:02 -0700

    100_million seedscript mod with url referencing localy stored images folder

--- data_generator/seed_data_csv.js
+++ data_generator/seed_data_csv.js
@@ -6,26 +6,51 @@ const zeroFill = require('zero-fill');
 const writer = csvWriter();
 
 
+// Math.floor(Math.random() * (12 - 9 + 1)) + 9;
+var numInRange = Math.floor(Math.random() * (12 - 9 + 1)) + 9;
 
 var randomImage = function () {
-    var num = Math.floor(Math.random() * 100) + 1;
-    return zeroFill(3, num);
-  }
+  var num = Math.floor(Math.random() * 1000) + 1;
+  return zeroFill(3, num);
+}
 
 writer.pipe(fs.createWriteStream('./sdc_seed_data_1m.csv'))
 
-var insertPhotoRow = function () {
+var insertPhotoRow = function (resId) {
+
     let imageName = randomImage();
     let url = `./images/00${imageName}.jpg`;
-
-    let restaurant_id = Math.floor(Math.random() * 100) + 1;
+    let restaurant_id = resId;
     let description = faker.lorem.sentence();
     let date = faker.date.past().toString();
     let source = faker.lorem.words();
-    var dataEntry = {url,restaurant_id,description,date,source}
 
+    var dataEntry = {url,restaurant_id,description,date,source}
     return dataEntry;
+  
 }
+
+
+
+
+
+
+
+
+var nineTo12Times = function (cd,input) {
+  var nineTo12 = Math.floor(Math.random() * (12 - 9 + 1)) + 9;
+  for(var i = 0; i < nineTo12; i++){
+    cd(input);
+  }
+}
+
+
+
+
+
+
+
+
 
 const write1mil = ()=>{
   let i = 10000000;
@@ -35,22 +60,24 @@ const write1mil = ()=>{
     do {
       i--
       if(i===0) {
-        var data = insertPhotoRow();
+        var data = insertPhotoRow(i);
 
         writer.write(data);
-      } else {
-        var data = insertPhotoRow();
 
+      } else {
+
+        var data = insertPhotoRow(i);
+        for(var j = 0; j < numInRange; j++){
+          writer.write(insertPhotoRow(i))
+        }
         ok = writer.write(data);
       }
-    } while (i > 0 && ok);
-    if (i>0) {
+    } while (i > 1 && ok);
+    if (i>1) {
 
       writer.once('drain', createPhotos)
     }
   } 
 }
-git
+
 write1mil()
-
-
